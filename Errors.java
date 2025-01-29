f94f7108eea7e26d1abd1cb8b8a1473f0d23b36d
varshith05082004 <varshithreddybobbali@gmail.com>
2025-01-29 21:42:28 +0530

    Add files via upload

---
+++ Errors.java
@@ -0,0 +1,88 @@
+// public class Errors {
+//     public static void main(String[] args) {
+//         int a=10;
+//         int b=0;
+//         System.out.println(a/b);
+//     }
+// }
+
+// public class Errors{
+//     public static void main(String[] args) {
+//         String str=null;
+//         System.out.println(str.length());
+//     }
+// }
+
+// public class Errors{
+//     public static void main(String[] args) {
+//         int[] num={1,2,3};
+//         System.out.println(num[3]);
+//     }
+// }
+
+// public class Errors{
+//     public static void main(String[] args) {
+//         int age=18;
+//         if age>=18
+//             System.out.println("Adult");
+//     }
+// }
+
+// public class Errors{
+//     public static void main(String[] args) {
+//         int[] num={1,2,3};
+//         for(int i=0;i<=num.length;i++){
+//             System.out.println(i);
+//         }
+//     }
+// }
+
+// public class Errors{
+//     public static void main(String[] args) {
+//         int n=10
+//         System.out.println(n);
+//     }
+// }
+
+// public class Errors{
+//     public static void main(String[] args) {
+//         int n=10;
+//         System.out.println(n);
+//     }
+// }
+
+// public class Errors{
+//     public static void main(String[] args) {
+//         int num="10";
+//         System.out.println(num);
+//     }
+// }
+
+// public class Errors{
+//     public static void main(String[] args) {
+//         int ;
+//         System.out.println();
+//     }
+// }
+
+// public class Errors{
+//     public static void main(String[] args) {
+//         System.out.println(m);
+//     }
+// }
+
+// public class Errors{
+//     public static void main(String[] args) {
+//         int n=24;
+//         if n%2==0;
+//         System.out.println("even );
+//     }
+// }
+
+// public class Errors{
+//     public static void main(String[] args) {
+//         for(i=0,i<5,i++){
+//             System.out.println(i);
+//         }
+//     }
+// }
